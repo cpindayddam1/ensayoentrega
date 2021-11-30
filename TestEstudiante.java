c2a826dee93a9d3d0df9019b5b2038de1600a409
cpindayddam1 <cpindayddam1@gmail.com>
2021-11-30 09:17:31 +0100

    Añadido readme.txt y modificado comentario de la clase TestEstudiante

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -1,6 +1,7 @@
 
 /**
- *  Para probar Git Github desde BlueJ
+ *  Probando git desde linea de comandos
+ *  
  *  
  */
 public class TestEstudiante
@@ -15,8 +16,8 @@ public class TestEstudiante
         e1.printEstudiante();
         Estudiante e2 = new Estudiante("Ana", 8);
         e2.printEstudiante();
-		Estudiante e3 = new Estudiante("Elisa", 26);
-		e3.printEstudiante();
+        Estudiante e3 = new Estudiante("Elisa", 26);
+        e3.printEstudiante();
 
     }
 }
